067ee8c0d719b0f9496f680b06eed2b51248898b
Paul Barrett <pmbarrett314@gmail.com>
2016-07-26 15:19:49 -0500

    Added dice rolling

--- reaction_count_bot/main.py
+++ reaction_count_bot/main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from reaction_count.reaction_count_bot import ReactionCountBot
+from roller.roller import Roller
 from slack_bot.default_log_handler import DefaultLogHandler
 from slack_bot.slack_bot import Bot
 import coloredlogs
@@ -30,14 +31,16 @@ def set_up_logging():
     debug_file_handler = logging.handlers.RotatingFileHandler(os.path.join(log_folder, "debug.txt"), maxBytes=(1024**2) / 2, backupCount=1)
     debug_file_handler.setLevel(logging.DEBUG)
 
-    logging.basicConfig(level=logging.DEBUG, handlers=[console_handler, file_handler, warning_file_handler, debug_file_handler])
+    logging.basicConfig(level=logging.INFO, handlers=[console_handler, file_handler, warning_file_handler, debug_file_handler])
 
 if __name__ == "__main__":
     set_up_logging()
     bot = Bot(get_api_key())
     plugin = ReactionCountBot(bot, reset_data=False)
     plugin2 = DefaultLogHandler(bot)
+    roller = Roller(bot)
     bot.register_plugin(plugin)
     bot.register_plugin(plugin2)
+    bot.register_plugin(roller)
 
     bot.run()
